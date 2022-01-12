e1f27a6b998a74c712097321d73ad155eabb3b23
nijuni2022 <nj@lionx.com.br>
2022-01-12 12:52:54 -0300

    alterei o nome do arquivo

--- insert.py
+++ insert.py
@@ -16,18 +16,26 @@ def conexaobanco():
 
 vcon=conexaobanco()
 
-vsql="""INSERT INTO tb_contatos
-            (T_NOMECONTATO,T_TELEFONECONTATO,T_EMAILCONTATO)
-        VALUES('teste_nome','teste_telefone','teste_email')"""
 
 def inserir(conexao,sql):
     try:
         c=conexao.cursor()
         c.execute(sql)
         conexao.commit()
-        print('Registro inserido')
+        #print('Registro inserido')
 
     except Error as ex:
         print(ex)
 
-inserir(vcon, vsql)
+r = 'S'
+
+while r == 'S':
+    nome = input('Digite o nome: ')
+    telefone = input('Digite o telefone: ')
+    email = input('Digite o email: ')
+
+    vsql="INSERT INTO tb_contatos(T_NOMECONTATO,T_TELEFONECONTATO,T_EMAILCONTATO)VALUES('"+nome+"','"+telefone+"','"+email+"')"
+    inserir(vcon, vsql)
+    r = str(input('Quer cadastrar outra pessoa? [S/N]')).upper()
+print('Registro Inserido')
+    
